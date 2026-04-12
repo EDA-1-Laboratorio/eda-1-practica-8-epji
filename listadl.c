1f58cba214f133c0a822780742428357cc013cc1
Padilla Huesca Edgar Ivan <edgar19ivan22@gmail.com>
2026-04-12 11:15:03 -0600

    Ejercicio completado

--- listadl.c
+++ listadl.c
@@ -1,5 +1,24 @@
 #include "listadl.h"
 
+
+
+int main() {
+    ListaDL *mi_lista = crear_lista();
+    
+    insertar_inicio(mi_lista, 10);
+    insertar_final(mi_lista, 20);
+    insertar_inicio(mi_lista, 5);
+    
+    printf("Lista Circular: \n");
+    imprimir_lista(mi_lista);
+    liberar_lista(mi_lista);
+
+    printf("Lista Reversa: \n");
+    imprimir_lista_reversa(mi_lista);
+    return 0;
+
+}
+
 dllista *crear_elemento(DATO dato) {
     dllista *nuevo = (dllista *)malloc(sizeof(dllista));
     if (nuevo == NULL)
@@ -21,40 +40,39 @@ ListaDL *crear_lista(void) {
 
 void insertar_inicio(ListaDL *lista, DATO dato) {
     dllista *nuevo = crear_elemento(dato);
-    if (nuevo == NULL)
-        return;
+    if (nuevo == NULL) return;
 
     if (lista->cabeza == NULL) {
-        nuevo -> siguiente = nuevo;
-        nuevo -> previo = nuevo;
         lista->cabeza = nuevo;
+        nuevo->siguiente = nuevo;
+        nuevo->previo = nuevo;
     } else {
-        dllista *ultimo = lista -> cabeza -> previo;
+        dllista *ultimo = lista->cabeza->previo;
         nuevo->siguiente = lista->cabeza;
-        nuevo -> previo = ultimo;
+        nuevo->previo = ultimo;
         lista->cabeza->previo = nuevo;
-        ultimo -> siguiente = nuevo;
+        ultimo->siguiente = nuevo;
         lista->cabeza = nuevo;
     }
     lista->longitud++;
 }
 
 void insertar_final(ListaDL *lista, DATO dato) {
-    dllista *nuevo = crear_elemento(dato);
-    if (nuevo == NULL)
-        return;
-
     if (lista->cabeza == NULL) {
-        lista->cabeza = nuevo;
-    } else {
-        dllista *actual = lista->cabeza;
-        while (actual->siguiente != NULL)
-            actual = actual->siguiente;
-        actual->siguiente = nuevo;
-        nuevo->previo = actual;
+        insertar_inicio(lista, dato);
+        return;
     }
+    dllista *nuevo = crear_elemento(dato);
+    if (nuevo == NULL) return;
+
+    dllista *ultimo = lista->cabeza->previo;
+    nuevo->siguiente = lista->cabeza;
+    nuevo->previo = ultimo;
+    ultimo->siguiente = nuevo;
+    lista->cabeza->previo = nuevo;
     lista->longitud++;
 }
+
 
 void insertar_en_posicion(ListaDL *lista, DATO dato, int posicion) {
     if (posicion < 0 || posicion > lista->longitud)
@@ -85,41 +103,35 @@ void insertar_en_posicion(ListaDL *lista, DATO dato, int posicion) {
 }
 
 DATO eliminar_inicio(ListaDL *lista) {
-    if (lista->cabeza == NULL)
-        return -1;
-
+    if (lista->cabeza == NULL) return -1;
     dllista *eliminado = lista->cabeza;
     DATO dato = eliminado->dato;
 
-    if (lista->cabeza->siguiente == NULL) {
+    if (lista->longitud == 1) {
         lista->cabeza = NULL;
     } else {
+        dllista *ultimo = lista->cabeza->previo;
         lista->cabeza = lista->cabeza->siguiente;
-        lista->cabeza->previo = NULL;
+        lista->cabeza->previo = ultimo;
+        ultimo->siguiente = lista->cabeza;
     }
-
     free(eliminado);
     lista->longitud--;
     return dato;
 }
 
 DATO eliminar_final(ListaDL *lista) {
-    if (lista->cabeza == NULL)
-        return -1;
+    if (lista->cabeza == NULL) return -1;
+    if (lista->longitud == 1) return eliminar_inicio(lista);
 
-    dllista *actual = lista->cabeza;
-    while (actual->siguiente != NULL)
-        actual = actual->siguiente;
+    dllista *ultimo = lista->cabeza->previo;
+    DATO dato = ultimo->dato;
+    dllista *penultimo = ultimo->previo;
 
-    DATO dato = actual->dato;
+    penultimo->siguiente = lista->cabeza;
+    lista->cabeza->previo = penultimo;
 
-    if (actual->previo == NULL) {
-        lista->cabeza = NULL;
-    } else {
-        actual->previo->siguiente = NULL;
-    }
-
-    free(actual);
+    free(ultimo);
     lista->longitud--;
     return dato;
 }
@@ -145,17 +157,6 @@ DATO eliminar_en_posicion(ListaDL *lista, int posicion) {
     return dato;
 }
 
-int buscar(ListaDL *lista, DATO dato) {
-    dllista *actual = lista->cabeza;
-    int posicion = 0;
-    while (actual != NULL) {
-        if (actual->dato == dato)
-            return posicion;
-        actual = actual->siguiente;
-        posicion++;
-    }
-    return -1;
-}
 
 DATO obtener(ListaDL *lista, int posicion) {
     if (posicion < 0 || posicion >= lista->longitud)
@@ -176,44 +177,65 @@ int longitud(ListaDL *lista) {
 }
 
 void imprimir_lista(ListaDL *lista) {
-    if(lista->cabeza == NULL){
+    if (lista->cabeza == NULL) {
         printf("lista vacia\n");
         return;
     }
-    dllista *actual = lista->cabeza; 
-        
-    while (actual != NULL) {
+    dllista *actual = lista->cabeza;
+    do {
         printf("[%d]", actual->dato);
-        if (actual->siguiente != NULL)
-            printf(" <-> ");
+        if (actual->siguiente != lista->cabeza) printf(" <-> ");
         actual = actual->siguiente;
-    }
-    printf(" -> NULL\n");
+    } while (actual != lista->cabeza);
+    printf(" <-> (Cabeza)\n");
+}
+
+int buscar(ListaDL *lista, DATO dato) {
+    if (lista->cabeza == NULL) return -1;
+    dllista *actual = lista->cabeza;
+    int pos = 0;
+    do {
+        if (actual->dato == dato) return pos;
+        actual = actual->siguiente;
+        pos++;
+    } while (actual != lista->cabeza);
+    return -1;
 }
 
 void imprimir_lista_reversa(ListaDL *lista) {
-    dllista *actual = lista->cabeza;
-    if (actual == NULL) {
-        printf(" -> NULL\n");
+    if (lista->cabeza == NULL) {
+        printf("lista vacia\n");
         return;
     }
-    while (actual->siguiente != NULL)
-        actual = actual->siguiente;
-    while (actual != NULL) {
+
+    // En circular, el anterior a la cabeza es el ÚLTIMO nodo
+    dllista *actual = lista->cabeza->previo; 
+    int i = 0;
+
+    printf("Lista Reversa: ");
+    while (i < lista->longitud) {
         printf("[%d]", actual->dato);
-        if (actual->previo != NULL)
+        
+        if (i < lista->longitud - 1) {
             printf(" <-> ");
+        }
+        
+        // Caminamos hacia atrás
         actual = actual->previo;
+        i++;
     }
-    printf(" -> NULL\n");
+    printf(" <-> (Atras)\n");
 }
 
 void liberar_lista(ListaDL *lista) {
-    dllista *actual = lista->cabeza;
-    while (actual != NULL) {
-        dllista *siguiente = actual->siguiente;
-        free(actual);
-        actual = siguiente;
+    if (lista->cabeza != NULL) {
+        dllista *actual = lista->cabeza;
+        int total = lista->longitud;
+        for (int i = 0; i < total; i++) {
+            dllista *sig = actual->siguiente;
+            free(actual);
+            actual = sig;
+        }
     }
     free(lista);
 }
